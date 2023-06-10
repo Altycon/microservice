20fad5247e07412a97de5fab81405b8646e7d490
Altycon <altycon1@gmail.com>
2023-06-10 04:06:53 -0700

    adding whoami api endpoint

--- server.js
+++ server.js
@@ -1,52 +1,61 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 const cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request,response)=>{
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/stamp_of_time', (request,response)=>{
+app.get('/stamp_of_time', (request, response) => {
     response.sendFile(__dirname + '/views/pages/stamp_of_time.html');
 });
 
 // your first API endpoint... 
-app.get("/api/:date?", (request, response)=>{
-  let date;
-  if(!request.params.date){
-        date = new Date();
-  }else{
-        date = request.params.date;
-  }
-    let timeChecked;
-    if(date == +date) timeChecked = +date;
-    else timeChecked = date;
-    
-    const newDate = new Date(timeChecked);
-    const unix = newDate.valueOf();
-    const utc = newDate.toUTCString();
+app.get("/api/:property?", (request, response) => {
 
-    if(isNaN(parseFloat(unix))){
-        response.json({ error: utc })
-    }else{
-        response.json({ unix: unix, utc: utc });
-    }
-  
-  
+  if(request.params.property === 'whoami'){
+
+      const ipaddress = request.ip;
+      const software = request.headers['user-agent'];
+      const language = request.headers['accept-language'];
+
+      response.json({ ipaddress: ipaddress, language: language, software: software });
+
+  }else{
+
+      let date;
+      if (!request.params.property) {
+          date = new Date();
+      } else {
+          date = request.params.property
+      }
+
+      let timeChecked;
+      if (date == +date) timeChecked = +date;
+      else timeChecked = date;
+
+      const newDate = new Date(timeChecked);
+      const unix = newDate.valueOf();
+      const utc = newDate.toUTCString();
+
+      if (isNaN(parseFloat(unix))) {
+          response.json({ error: utc })
+      } else {
+          response.json({ unix: unix, utc: utc });
+      }
+  }
 });
 
-
-
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
